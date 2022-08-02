c31485b48a9926b1ea087bff76247c70b9d5997b
obafemitope50 <obafemitope50@gmail.com>
2022-08-02 13:41:57 -0700

    contains forms.py, urls.py, views.py and the login_url in settings.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    username = models.TextField(unique=True)
+    email = models.EmailField(unique=True)
+    
+    
+        
+	
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = "/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
