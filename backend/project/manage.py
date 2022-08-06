cd961cc983ccaad22127e81f4f70a56f48fc8fa5
Ekwugha Clinton <eclinton011@gmail.com>
2022-08-06 12:29:37 +0100

    project

--- backend/project/manage.py
+++ backend/project/manage.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-        main()
+    main()
---
+++ backend/project/project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from user import views as user_view
+from django.contrib.auth import views as auth
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('user.urls')),
+    path('login/', user_view.login, name = 'login'),
+    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
+    path('register/', user_view.register, name ='register'),
+]
